def362f94884194f4bab7e3ab9546fd5991e4cda
lawanaji <lawanmustaphaaji2015@gmail.com>
2025-04-28 20:34:48 +0100

    fix bug

--- src/app/components/skeleton/skeleton.tsx
+++ src/app/components/skeleton/skeleton.tsx
@@ -1,8 +1,8 @@
 const ProductCardSkeleton = () => {
   return (
-    <div className="bg-gray-200 rounded-lg shadow-md overflow-hidden animate-pulse h-full">
+    <div className="bg-gray-200 rounded-lg shadow-md overflow-hidden animate-pulse h-full w-52 space-x-4">
       <div className="p-4">
-        <div className="flex justify-between items-center mb-2">
+        <div className="flex justify-between items-center mb-2 p-4">
           <div className="w-10 h-6 bg-gray-300 rounded-md"></div>
           <div className="flex gap-2">
             <div className="w-5 h-5 bg-gray-300 rounded-full"></div>
